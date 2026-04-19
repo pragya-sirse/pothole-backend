ce77abec3d5818cb47de6fc2a90ca5dd30ddb5fa
pragya <pragyasirse@gmail.com>
2026-04-19 22:36:34 +0530

    fix cors config in security config

--- src/main/java/com/pothole/pothole_backend/config/SecurityConfig.java
+++ src/main/java/com/pothole/pothole_backend/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
-                .cors(cors -> {})
+                .cors(cors -> cors.configure(http))
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .headers(headers ->

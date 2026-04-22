0534d9edaae1ce13b8c37b7ad3e21bf62313e70b
pragya <pragyasirse@gmail.com>
2026-04-22 08:25:59 +0530

    Fix LocalDateTime serialization and Objectmapper

--- src/main/java/com/pothole/pothole_backend/config/AppConfig.java
+++ src/main/java/com/pothole/pothole_backend/config/AppConfig.java
@@ -1,6 +1,8 @@
 package com.pothole.pothole_backend.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -16,6 +18,9 @@ public class AppConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return mapper;
     }
 }
